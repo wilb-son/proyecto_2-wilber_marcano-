4c20eeba00e0ac01ec022ef263b161abc598d87c
wilson <wilbermarcano04@gmail.com>
2024-11-23 10:53:47 -0500

    hice un recorrido de los libros

--- libreria.js
+++ libreria.js
@@ -1,1 +1,34 @@
 //hola //
+const librosColeccionJSON=[
+    {   "Nombre":"11 minutos",
+        "CantidadDePaginas":180, 
+        "Autor":"paulo coelho",
+        "Género":["Novela, Ficción"],
+        "disponible":true
+    },
+    {
+        "Nombre":"el alquimista",
+        "CantidadDePaginas":"240",
+        "Autor":"paulo coelho",
+        "Género":"Novela, Drama, Misión, Literatura fantástica, Ficción de aventuras",
+        "disponible":false
+    }
+    ]
+    function informacionLibro(libro ) {
+        
+        console.log("Título:", libro.Nombre);
+        console.log("cantidad de paginas",libro.CantidadDePaginas);
+        console.log("Autor:", libro.Autor);
+        console.log("genero:",  libro.Género);
+        console.log("disponible", libro.disponible);
+    }
+    
+
+
+function  imprimirLibros(arregloLibros) {
+    for (let i = 0; i < arregloLibros.length; i++) {
+        const libro = arregloLibros[i];
+       informacionLibro(libro);
+    }
+}
+imprimirLibros(librosColeccionJSON);
